821ccbed66d7095172f6e0920aec93f48a6425e4
CC.Client <CC.Client@D1566-W10>
2018-01-04 18:36:11 +0530

    Add project files.

---
+++ TestService/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceProcess;
+
+namespace TestService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1(),
+                new Service2()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ TestService/TestServiceInstaller.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    [RunInstaller(true)]
+    public partial class TestServiceInstaller : Installer
+    {
+        public TestServiceInstaller()
+        {
+            
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+        }
+
+        //void Commit(IDictionary savedState)
+        //{
+        //    base.Commit(savedState);
+        //}
+
+        //void Rollback(IDictionary savedState)
+        //{
+
+        //}
+
+        //void OnAfterInstall(IDictionary savedState)
+        //{
+
+        //}
+        //void OnAfterRollback(IDictionary savedState)
+        //{
+
+        //}
+        //void OnAfterUninstall(IDictionary savedState)
+        //{
+
+        //}
+        //void OnBeforeInstall(IDictionary savedState)
+        //{
+
+        //}
+        //void OnBeforeRollback(IDictionary savedState)
+        //{
+        //}
+        //void OnBeforeUninstall(IDictionary savedState)
+        //{
+
+        //}
+        //void OnCommitted(IDictionary savedState)
+        //{
+
+        //}
+        //void OnCommitting(IDictionary savedState)
+        //{
+
+        //}
+    }
+}
